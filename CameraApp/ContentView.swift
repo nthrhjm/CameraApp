50ec1a47329d9505b3d0cf3c9970c469f40b87fb
Hajime Nutahara <hajime.nutahara@gmail.com>
2020-07-07 01:41:48 +0900

    add ImagePicker to open phot library

--- CameraApp/ContentView.swift
+++ CameraApp/ContentView.swift
@@ -31,7 +31,7 @@ struct ContentView: View {
             }
             //カメラ画面を表示するコード
             if isPicking {
-                Rectangle()
+                ImagePicker()
                     .edgesIgnoringSafeArea(.all)
                     .transition(.move(edge: .bottom))
                     .animation(.easeInOut)
---
+++ ImagePicker.swift
@@ -0,0 +1,26 @@
+//
+//  ImagePicker.swift
+//  CameraApp
+//
+//  Created by nhajime on 2020/07/07.
+//  Copyright © 2020 nhajime. All rights reserved.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+    }
+}
+
+struct ImagePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        ImagePicker()
+    }
+}
