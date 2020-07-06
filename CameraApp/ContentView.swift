dbeba432de30e8f4e197b3fae52c50b47369621e
Hajime Nutahara <hajime.nutahara@gmail.com>
2020-07-07 00:59:09 +0900

    add layout

--- CameraApp/ContentView.swift
+++ CameraApp/ContentView.swift
@@ -9,10 +9,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var image: Image?
+    @State var isPicking = false
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            VStack {
+                VStack { //撮影した写真を表示するコード
+                    Spacer()
+                    image?
+                        .resizable()
+                        .scaledToFit()
+                    Spacer()
+                }
+                HStack { //カメラボタンを表示するコード
+                    Spacer()
+                    Button(action: { self.isPicking = true }) {
+                        Image(systemName: "camera")
+                        Text("カメラ")
+                    }.padding()
+                }
+            }
+            //カメラ画面を表示するコード
+            if isPicking {
+                Rectangle()
+                    .edgesIgnoringSafeArea(.all)
+                    .transition(.move(edge: .bottom))
+                    .animation(.easeInOut)
+            }
+        }
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
